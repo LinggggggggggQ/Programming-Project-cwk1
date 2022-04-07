cdcf83af738656745505144adf06395bef8eec6e
Ling <1953734432@qq.com>
2022-04-07 14:49:38 +0800

    last

---
+++ interface.h
@@ -0,0 +1,8 @@
+#ifndef INTERFACE_GUARD__H
+#define INTERFACE_GUARD__H
+#define Max 1000
+
+
+void run_interface(); //Initial interface of operation
+void user_interface(char *name);//User Interface
+#endif
