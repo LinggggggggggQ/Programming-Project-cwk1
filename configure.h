d274915f00ade63a98fac4cfa8f0cd7c0d7feabb
Ling <1953734432@qq.com>
2022-04-07 14:49:20 +0800

    last

---
+++ configure.h
@@ -0,0 +1,9 @@
+
+#ifndef LIBRARY_CONFIGURE_H
+#define LIBRARY_CONFIGURE_H
+
+#define PATH_BOOK_MANAGEMENT "C:\\Users\\19537\\Desktop\\cwk2library\\book_management.txt"
+#define PATH_USER_BOOK "C:\\Users\\19537\\Desktop\\cwk2library\\user_book.txt"
+#define  PATH_USERINFO "C:\\Users\\19537\\Desktop\\cwk2library\\Userinfo.txt"
+
+#endif //LIBRARY_CONFIGURE_H
