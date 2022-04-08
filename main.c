3e5bf44f52cce24596ebecba26d13e8ddd2bb2bc
Ling <1953734432@qq.com>
2022-04-08 17:17:14 +0800

    trylast

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
+#include <stdio.h>
 #include "interface.h"
+
+
+#ifdef PARAM_SUP
+char* _BOOK_DATA_PATH = NULL;
+char* _USER_INFO_PATH = NULL;
+char* _USER_BOOK_PATH = NULL;
+#endif
 
 int main(int argc, char const *argv[])
 {
+#ifdef PARAM_SUP
+    if (argc == 4) {
+        _BOOK_DATA_PATH = argv[1];
+        _USER_INFO_PATH = argv[2];
+        _USER_BOOK_PATH = argv[3];
+    }  else {
+        printf("Invalid parameters.\n");
+        return -1;
+    }
+#endif
     run_interface();//调用interface中的run_interface函数，展示用户选择界面
     return 0;
 }
